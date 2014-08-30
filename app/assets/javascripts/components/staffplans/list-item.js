db74842846af136710f068a8a05d55685f37fdc1
Rob Sterner <rob@rescuedcode.com>
2014-08-29 21:16:10 -0400

    Tweaked some CSS. Added the totals for assignments on staffplans#index.

--- app/assets/javascripts/components/staffplans/list-item.js
+++ app/assets/javascripts/components/staffplans/list-item.js
@@ -3,23 +3,25 @@ function ListItemViewModel(data) {
   this.staffPlanURL = "/staffplans/" + this.data.user.id
   this.observedWorkWeeks = ko.observableArray();
   this.observedWorkWeeks.extend({rateLimit: 25});
-  
+
   this.visibleWorkWeeks = ko.computed(function() {
     return _.map(this.data.weekRange(), function(weekData, index) {
-      
+
       var userWorkWeek = _.find(this.data.user.work_weeks, function(workWeek) {
           return workWeek.cweek == weekData.cweek() && workWeek.year == weekData.year();
         }, this);
-      
+
       if(_.isUndefined(this.observedWorkWeeks()[index])) {
         // add to the set
         var date = moment(weekData.beginning_of_week());
-        
+
         this.observedWorkWeeks()[index] = {
             cweek: ko.observable(weekData.cweek())
           , year: ko.observable(weekData.year())
           , actual: ko.observable(0)
           , estimated: ko.observable(0)
+          , estimated_proposed: ko.observable(0)
+          , estimated_planned: ko.observable(0)
           , beginning_of_week: ko.observable(weekData.beginning_of_week())
         }
       } else {
@@ -28,26 +30,36 @@ function ListItemViewModel(data) {
         this.observedWorkWeeks()[index].year(weekData.year())
         this.observedWorkWeeks()[index].beginning_of_week(weekData.beginning_of_week())
       }
-      
+
       // add user data if available
       if(_.isUndefined(userWorkWeek)) {
         this.observedWorkWeeks()[index].actual(0);
         this.observedWorkWeeks()[index].estimated(0);
+        this.observedWorkWeeks()[index].estimated_planned(0);
+        this.observedWorkWeeks()[index].estimated_proposed(0);
       } else {
         this.observedWorkWeeks()[index].actual(userWorkWeek.actual);
         this.observedWorkWeeks()[index].estimated(userWorkWeek.estimated);
+        this.observedWorkWeeks()[index].estimated_planned(userWorkWeek.estimated_planned);
+        this.observedWorkWeeks()[index].estimated_proposed(userWorkWeek.estimated_proposed);
       }
-      
+
       return this.observedWorkWeeks()[index];
     }, this);
   }, this);
   this.visibleWorkWeeks.extend({rateLimit: 25});
-  // this.computeVisibleWorkWeeks();
 }
 
 _.extend(ListItemViewModel.prototype, {
-  computeVisibleWorkWeeks: function() {
-    
+  totalEstimatedHours: function() {
+    return _.reduce(this.data.user.work_weeks, function(sum, workWeek) {
+      return sum += (workWeek.estimated || 0);
+    }, 0);
+  },
+  totalActualHours: function() {
+    return _.reduce(this.data.user.work_weeks, function(sum, workWeek) {
+      return sum += (workWeek.actual || 0);
+    }, 0);
   }
 });
 
--- app/serializers/staffplan_list_serializer.rb
+++ app/serializers/staffplan_list_serializer.rb
@@ -1,42 +1,43 @@
 class StaffplanListSerializer < ActiveModel::Serializer
   attributes :id, :gravatar_url, :staffplan_url, :full_name, :email, :work_weeks, :estimated_total, :actual_total, :diff
-  
+
   def full_name
     "#{object.first_name} #{object.last_name}"
   end
-  
+
   def staffplan_url
     staffplans_path(object)
   end
-  
+
   def gravatar_url
     "https://secure.gravatar.com/avatar/#{Digest::MD5.hexdigest(email.downcase)}"
   end
-  
+
   def work_weeks
     object.staffplans_list_views.group_by {|slv| {cweek: slv.cweek, year: slv.year}}.map do |key, values|
       key.merge(
         beginning_of_week: values.first.beginning_of_week,
         estimated: values.inject(0) {|sum, value| sum += (value.estimated_total || 0)},
         actual: values.inject(0) {|sum, value| sum += (value.actual_total || 0)},
-        proposed: values.map(&:proposed).uniq.first
+        estimated_proposed: values.select(&:proposed).inject(0) {|sum, value| sum += (value.estimated_total || 0)},
+        estimated_planned: values.reject(&:proposed).inject(0) {|sum, value| sum += (value.estimated_total || 0)}
       )
     # TODO: use a SortedSet here?
     end.sort { |x,y| x[:beginning_of_week] <=> y[:beginning_of_week] }
   end
-  
+
   def estimated_total
     object.staffplans_list_views.inject(0) { |sum, slv| sum += (slv.estimated_total || 0) }
   end
-  
+
   def actual_total
     object.staffplans_list_views.inject(0) { |sum, slv| sum += (slv.actual_total || 0) }
   end
-  
+
   def diff
     object.staffplans_list_views.inject(0) { |sum, slv| sum += (slv.diff || 0) }
   end
-  
+
   def url
     "/staffplans/#{object.id}"
   end
