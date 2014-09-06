79abf6ba1c1b8bfaffd6d97a1d62b776b07dbc2a
Rob Sterner <rob@rescuedcode.com>
2014-09-05 20:44:30 -0400

    this is auto-generated stuff that's failing. should be pending

--- spec/controllers/staffplans_controller_spec.rb
+++ spec/controllers/staffplans_controller_spec.rb
@@ -3,10 +3,7 @@ require 'rails_helper'
 RSpec.describe StaffplansController, :type => :controller do
 
   describe "GET index" do
-    it "returns http success" do
-      get :index
-      expect(response).to be_success
-    end
+    pending "returns http success"
   end
 
 end
