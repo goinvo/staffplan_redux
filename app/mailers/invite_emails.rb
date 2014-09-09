29999621422e9a7ce05decc9d21c050d6b565a21
craigmcginley <craigmcginley@gmail.com>
2014-09-09 16:55:50 -0400

    updated email messages, and cleaned up sending process

--- app/mailers/invite_emails.rb
+++ app/mailers/invite_emails.rb
@@ -6,18 +6,18 @@ class InviteEmails < ActionMailer::Base
   #
   #   en.invites.invite.subject
   #
-  def existing_user_invite(email, sender)
-    @sender = sender
+  def existing_user_invite(invite)
+    @invite = invite
 
-    mail to: email,
-      subject: "Invitation from #{sender.full_name} to join a company on StaffPlan"
+    mail to: invite.email,
+      subject: "Invitation from #{invite.sender.full_name} to join #{invite.company.name} on StaffPlan"
   end
 
-  def new_user_invite(email, sender)
-    @sender = sender
+  def new_user_invite(invite)
+    @invite = invite
 
-    mail to: email,
-      subject: "Invitation from #{sender.full_name} to join a company on StaffPlan"
+    mail to: invite.email,
+      subject: "Invitation from #{invite.sender.full_name} to join #{invite.company.name} on StaffPlan"
   end
 
   def response_email(user, invite)
@@ -25,6 +25,6 @@ class InviteEmails < ActionMailer::Base
     @invite = invite
 
     mail to: invite.sender.email,
-      subject: "Invite was responded to"
+      subject: "Invitation for #{invite.company.name} was responded to"
   end
 end
