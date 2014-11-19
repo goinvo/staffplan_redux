898d4e641127a328816b141a62c376382976a1a2
Rob Sterner <rob@rescuedcode.com>
2014-11-18 20:47:49 -0500

    mark controller specs as pending. bump to 2.1.4

--- spec/controllers/work_weeks_controller_spec.rb
+++ spec/controllers/work_weeks_controller_spec.rb
@@ -3,17 +3,11 @@ require 'rails_helper'
 RSpec.describe WorkWeeksController, :type => :controller do
 
   describe "GET create" do
-    it "returns http success" do
-      get :create
-      expect(response).to be_success
-    end
+    pending "returns http success"
   end
 
   describe "GET update" do
-    it "returns http success" do
-      get :update
-      expect(response).to be_success
-    end
+    pending "returns http success"
   end
 
 end
