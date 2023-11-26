9281174297b1b0f5ebfc08ecef759cd1b88b3b76
Rob Sterner <fermion@users.noreply.github.com>
2023-11-26 11:23:05 -0500

    delete a bunch of tests being generated, turn off rails' test framework

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,14 @@
 require_relative "boot"
 
-require "rails/all"
+require "active_record/railtie"
+require "active_storage/engine"
+require "action_controller/railtie"
+require "action_view/railtie"
+require "action_mailer/railtie"
+require "active_job/railtie"
+require "action_cable/engine"
+require "action_mailbox/engine"
+require "action_text/engine"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
