bd5f84a6ad8ee1eec5ba61d8a0e9723a2a5d9642
Rob Sterner <fermion@users.noreply.github.com>
2023-11-26 12:42:20 -0500

    the start of a sensible dev seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,34 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+# TODO: write a command object for this.
+
+goinvo = Company.find_or_create_by(name: "GoInvo")
+
+if goinvo.users.none?
+  rob = User.build(name: "Rob", email: "rob@example.com", current_company: goinvo)
+  juhan = User.build(name: "Juhan", email: "juhan@example.com", current_company: goinvo)
+  goinvo.memberships.build(user: rob, role: "owner", status: 'active')
+  goinvo.memberships.build(user: juhan, role: "owner", status: 'active')
+  goinvo.save!
+else
+  puts "Found users for GoInvo, skipping..."
+end
+
+if goinvo.clients.none?
+  design_anonymous = goinvo.clients.create(name: "Design Anonymous")
+  builders_paradise = goinvo.clients.create(name: "Builder's Paradise")
+
+  [design_anonymous, builders_paradise].each do |client|
+    5.times do
+      client.projects.create(
+        name: Faker::Company.name,
+        cost: Faker::Number.decimal(l_digits: 2),
+        payment_frequency: "monthly"
+      )
+    end
+  end
+else
+  puts "Found clients for GoInvo, skipping clients and projects..."
+end
