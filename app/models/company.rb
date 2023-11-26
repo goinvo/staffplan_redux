749f3addf9371912508423ffcc22ca5d619c4410
Rob Sterner <fermion@users.noreply.github.com>
2023-11-26 12:42:09 -0500

    adds some missing company associations

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,6 +1,8 @@
 class Company < ApplicationRecord
   has_many :memberships, dependent: :destroy
   has_many :users, through: :memberships
+  has_many :clients, dependent: :destroy
+  has_many :projects, through: :clients
 
   validates :name, presence: true, uniqueness: { case_sensitive: false }
 end
