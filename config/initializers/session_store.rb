5f7790b02bd4fe57620ef9e378f923086fd9df8e
Rob Sterner <rob@prettygood.software>
2024-01-02 20:44:36 -0500

    thought I accepted this Copilot suggstion

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,1 +1,1 @@
-Rails.application.config.session :cookie_store, key: '_staffplan_redux_session', domain: :all, tld_length: 2
+Rails.application.config.session_store :cookie_store, key: '_staffplan_redux_session', domain: :all, tld_length: 2
