fae174d7ee4c4399cf8c3fd2051f8b5c93b98f2e
Rob Sterner <rob@prettygood.software>
2024-01-04 21:22:48 -0500

    always mount graphiql for now

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,10 @@ Rails.application.routes.draw do
   namespace :api do
     get 'current_user/create'
   end
-  if Rails.env.development?
+
+  # if Rails.env.development?
     mount GraphiQL::Rails::Engine, at: "/graphiql", graphql_path: "/graphql"
-  end
+  # end
 
   post "/graphql", to: "graphql#execute"
 
