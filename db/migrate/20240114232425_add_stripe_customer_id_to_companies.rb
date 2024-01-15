e8842932ac8ae40662873d97df18f959a109093f
Rob Sterner <rob@prettygood.software>
2024-01-15 14:27:34 -0500

    adds stripe_id to customers

---
+++ db/migrate/20240114232425_add_stripe_customer_id_to_companies.rb
@@ -0,0 +1,5 @@
+class AddStripeCustomerIdToCompanies < ActiveRecord::Migration[7.1]
+  def change
+    add_column :companies, :stripe_id, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_01_14_005440) do
+ActiveRecord::Schema[7.1].define(version: 2024_01_14_232425) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -38,6 +38,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_01_14_005440) do
     t.string "name", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "stripe_id"
     t.index ["name"], name: "index_companies_on_name", unique: true
   end
 
