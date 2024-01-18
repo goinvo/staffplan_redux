0f9856982bd37c4ed0c1186d1260ded9f6839b69
Rob Sterner <rob@prettygood.software>
2024-01-18 09:56:13 -0500

    constantize things

--- app/models/membership.rb
+++ app/models/membership.rb
@@ -5,18 +5,20 @@ class Membership < ApplicationRecord
   OWNER = 'owner'.freeze
   ADMIN = 'admin'.freeze
   MEMBER = 'member'.freeze
+  ACTIVE = 'active'.freeze
+  INACTIVE = 'inactive'.freeze
 
   VALID_ROLES = [OWNER, ADMIN, MEMBER].freeze
-  VALID_STATUSES = %w[active inactive].freeze
+  VALID_STATUSES = [ACTIVE, INACTIVE].freeze
 
   validates :user, presence: true, uniqueness: { scope: :company }
   validates :company, presence: true, uniqueness: { scope: :user }
   validates :status, presence: true, inclusion: { in: VALID_STATUSES }
   validates :role, presence: true, inclusion: { in: VALID_ROLES }
 
-  scope :active, -> { where(status: 'active') }
+  scope :active, -> { where(status: Membership::ACTIVE) }
 
   def active?
-    status == 'active'
+    status == Membership::ACTIVE
   end
 end
