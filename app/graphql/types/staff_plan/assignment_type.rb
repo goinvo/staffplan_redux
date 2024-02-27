4eedca40d5a1ca3a04aa8f46b6ee6f8dee1d0d77
Rob Sterner <rob@prettygood.software>
2024-02-27 16:29:39 -0500

    renames Assignment.user to .assignedUser, adds some descriptions as well.

--- app/graphql/types/staff_plan/assignment_type.rb
+++ app/graphql/types/staff_plan/assignment_type.rb
@@ -4,15 +4,15 @@ module Types
   module StaffPlan
     class AssignmentType < Types::BaseObject
       field :id, ID, null: false
-      field :user, Types::StaffPlan::UserType, null: false
-      field :project, Types::StaffPlan::ProjectType, null: false
-      field :status, String, null: false
-      field :starts_on, GraphQL::Types::ISO8601Date, null: true
-      field :ends_on, GraphQL::Types::ISO8601Date, null: true
+      field :assigned_user, Types::StaffPlan::UserType, null: false, description: 'The user assigned to this assignment'
+      field :project, Types::StaffPlan::ProjectType, null: false, description: 'The project this assignment is for'
+      field :status, String, null: false, description: 'The status of the assignment'
+      field :starts_on, GraphQL::Types::ISO8601Date, null: true, description: 'The date the assignment starts'
+      field :ends_on, GraphQL::Types::ISO8601Date, null: true, description: 'The date the assignment ends'
       field :created_at, GraphQL::Types::ISO8601DateTime, null: false
       field :updated_at, GraphQL::Types::ISO8601DateTime, null: false
 
-      field :work_weeks, [Types::StaffPlan::WorkWeekType], null: false
+      field :work_weeks, [Types::StaffPlan::WorkWeekType], null: false, description: 'The work weeks for this assignment'
 
       def work_weeks
         object.work_weeks
