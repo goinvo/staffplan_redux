86859e362d472f536a328597effdf3423e5476c1
Rob Sterner <rob@prettygood.software>
2024-03-09 10:10:15 -0500

    adds in missing scoping that could cause company info to leak

--- app/graphql/types/staff_plan/user_type.rb
+++ app/graphql/types/staff_plan/user_type.rb
@@ -10,22 +10,22 @@ module Types
       field :created_at, GraphQL::Types::ISO8601DateTime, null: false
       field :updated_at, GraphQL::Types::ISO8601DateTime, null: false
 
-      field :companies, [Types::StaffPlan::CompanyType], null: false
+      field :companies, [Types::StaffPlan::CompanyType], null: false, description: "Fetches all companies for the current user."
 
       def companies
         object.companies
       end
 
-      field :assignments, [Types::StaffPlan::AssignmentType], null: false
+      field :assignments, [Types::StaffPlan::AssignmentType], null: false, description: "Fetches all of the user's assignments for the current company."
 
       def assignments
-        object.assignments
+        object.assignments.where(project_id: object.current_company.projects.map(&:id))
       end
 
-      field :projects, [Types::StaffPlan::ProjectType], null: false
+      field :projects, [Types::StaffPlan::ProjectType], null: false, description: "Fetches all projects for the current user for the current company."
 
       def projects
-        object.projects
+        object.projects.where(client_id: object.current_company.clients.map(&:id))
       end
     end
   end
