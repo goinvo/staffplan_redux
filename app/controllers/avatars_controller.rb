2a39d8d2184f0a00673269043798318e4c89b375
Rob Sterner <rob@prettygood.software>
2024-03-30 12:03:09 -0400

    migrate avatar management to a component

--- app/controllers/avatars_controller.rb
+++ app/controllers/avatars_controller.rb
@@ -2,7 +2,7 @@ class AvatarsController < ApplicationController
   before_action :require_user!
   def destroy
     current_user.avatar.purge if current_user.avatar.attached?
-    flash[:success] = "Custom avatar deleted successfully. Gravatar will be used."
+    flash[:success] = "Custom avatar deleted successfully."
     redirect_to users_profile_path
   end
 end
