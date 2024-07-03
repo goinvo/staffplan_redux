6878111e52e8d9f6fd5c381d3e66616d2bc87511
Rob Sterner <rob@prettygood.software>
2024-07-03 12:56:08 -0400

    point to the staffplan subdomain for the UI

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,7 +1,7 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     origins "http://localhost:8080"
-      "https://staffplan-ui.fermion.dev"
+      "https://ui.staffplan.com"
 
     resource "*",
              headers: :any,
