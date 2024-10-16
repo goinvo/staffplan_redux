ce3e93180e9b34d41a96defe17987bdfa4d83918
Rob Sterner <rob@prettygood.software>
2024-10-15 21:21:57 -0400

    revert passwordless-based redirect, send to dashboard#show. try to tighten up stripe redirect

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,4 +9,14 @@ class SessionsController < Passwordless::SessionsController
       redirect_to auth_sign_in_url, alert: "Sorry, please try that again."
     end
   end
+
+  def my_staffplan_url(current_user)
+    case Rails.env
+    when "production"
+      "https://ui.staffplan.com/people/#{current_user.id}"
+    else
+      "http://localhost:8080/people/#{current_user.id}"
+    end
+  end
+  helper_method :my_staffplan_url
 end
--- config/initializers/passwordless.rb
+++ config/initializers/passwordless.rb
@@ -8,15 +8,8 @@ Passwordless.configure do |config|
   config.timeout_at = lambda { 10.minutes.from_now } # How long until a token/magic link times out.
 
   config.redirect_back_after_sign_in = true # When enabled the user will be redirected to their previous page, or a page specified by the `destination_path` query parameter, if available.
-  config.redirect_to_response_options = {} # Additional options for redirects.
-  config.success_redirect_path = -> (current_user) {
-    case Rails.env
-    when 'production'
-      "https://ui.staffplan.com/people/#{current_user.id}"
-    else
-      "http://localhost:8080/people/#{current_user.id}"
-    end
-  }
+  config.redirect_to_response_options = {}
+  config.success_redirect_path = "/"
   config.failure_redirect_path = '/' # After a sign in fails
   config.sign_out_redirect_path = '/' # After a user signs out
   config.paranoid = true
