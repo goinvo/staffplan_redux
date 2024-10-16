6f566fe8453a1461751bd105fe36e83603c751a5
Rob Sterner <rob@prettygood.software>
2024-10-15 20:18:24 -0400

    redirect to my staffplan from /

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,6 +2,7 @@ class DashboardController < ApplicationController
   before_action :require_user!
 
   def show
+    redirect_to my_staffplan_url, allow_other_host: true
   end
 
   def switch_account
