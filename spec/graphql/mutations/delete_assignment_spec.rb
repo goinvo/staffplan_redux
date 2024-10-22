18c17eff1dfbec0748bd7f5126095f7f67111d83
Rob Sterner <rob@prettygood.software>
2024-10-21 20:02:10 -0400

    adds check on hours recorded against an assignment being deleted

--- spec/graphql/mutations/delete_assignment_spec.rb
+++ spec/graphql/mutations/delete_assignment_spec.rb
@@ -38,7 +38,7 @@ RSpec.describe Mutations::DeleteAssignment do
       expect { assignment.reload }.to raise_error(ActiveRecord::RecordNotFound)
     end
 
-    it "returns an error when a deletion is attempted on a non-TBD assignment" do
+    it "allows deletion on a non-TBD assignment with no hours recorded" do
       query_string = <<-GRAPHQL
         mutation($assignmentId: ID!) {
           deleteAssignment(assignmentId: $assignmentId) {
@@ -49,6 +49,7 @@ RSpec.describe Mutations::DeleteAssignment do
       GRAPHQL
 
       assignment = create(:assignment)
+      create(:work_week, assignment: assignment, actual_hours: 0)
       company = assignment.project.company
       user = company.users.first
 
@@ -63,9 +64,45 @@ RSpec.describe Mutations::DeleteAssignment do
         }
       )
 
+      expect(result["errors"]).to eq(nil)
+      expect {
+        assignment.reload
+      }.to raise_error(ActiveRecord::RecordNotFound)
       post_result = result["data"]["deleteAssignment"]
-      expect(result["errors"].first["message"]).to eq("Cannot delete an assignment that's assigned. Try archiving the assignment instead.")
-      assignment.reload
+      expect(post_result["id"]).to eq(assignment.id.to_s)
+      expect(post_result["status"]).to eq(Assignment::ACTIVE)
+    end
+
+    it "blocks deletion on an assigned assignment with hours recorded" do
+      query_string = <<-GRAPHQL
+        mutation($assignmentId: ID!) {
+          deleteAssignment(assignmentId: $assignmentId) {
+            id
+            status
+          }
+        }
+      GRAPHQL
+
+      assignment = create(:assignment)
+      create(:work_week, assignment: assignment, actual_hours: 15)
+      expect(assignment.reload.work_weeks.count).to eq(1)
+      company = assignment.project.company
+      user = company.users.first
+
+      result = StaffplanReduxSchema.execute(
+        query_string,
+        context: {
+          current_user: user,
+          current_company: company
+        },
+        variables: {
+          assignmentId: assignment.id
+        }
+      )
+
+      expect(result["errors"].first["message"]).to eq("Cannot delete an assignment that's assigned with hours recorded. Try archiving the assignment instead.")
+      expect(assignment.reload.persisted?).to eq(true)
+      post_result = result["data"]["deleteAssignment"]
       expect(post_result["id"]).to eq(assignment.id.to_s)
       expect(post_result["status"]).to eq(Assignment::ACTIVE)
     end
