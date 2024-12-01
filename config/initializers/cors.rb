798527bd0c5d6f8f97814064646c42a5c33ba44f
Rob Sterner <rob@prettygood.software>
2024-12-01 13:52:48 -0500

    trying to fix the cors issue

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,9 +1,9 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "http://localhost:8080"
-      "https://ui.staffplan.com"
+    origins "localhost:8080"
+      "ui.staffplan.com"
 
-    resource "*",
+    resource "/graphql",
              headers: :any,
              methods: [:get, :post, :put, :patch, :delete, :options, :head],
              credentials: true,
