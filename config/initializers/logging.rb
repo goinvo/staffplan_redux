e8b2f1f1b5ec16777b91ed3c65268b40d67b7b4b
Rob Sterner <rob@prettygood.software>
2024-12-14 14:48:22 -0500

    not seeing logs in prod

--- config/initializers/logging.rb
+++ config/initializers/logging.rb
@@ -1,7 +1,11 @@
 SemanticLogger.sync!
 SemanticLogger.default_level = :trace # Prefab will take over the filtering
-SemanticLogger.add_appender(
-  file_name: "log/#{Rails.env}.log",
+
+appender_args = {
   formatter: :color,
   filter: Prefab.log_filter,
-)
+}
+appender_args.merge!(io: $stdout) if Rails.env.production?
+appender_args.merge!(file_name: "log/#{Rails.env}.log") if Rails.env.development?
+
+SemanticLogger.add_appender(**appender_args)
