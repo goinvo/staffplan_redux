c99f44597f685deec7751173ca350e9192666247
Rob Sterner <rob@prettygood.software>
2025-01-04 09:47:51 -0500

    keep sidekiq for now, replace jobs gradually

--- config/puma.rb
+++ config/puma.rb
@@ -33,7 +33,8 @@ pidfile ENV.fetch("PIDFILE") { "tmp/pids/server.pid" }
 
 # Allow puma to be restarted by `bin/rails restart` command.
 plugin :tmp_restart
-plugin :solid_queue
+
+# plugin :solid_queue
 
 on_worker_boot do
   Prefab.fork
